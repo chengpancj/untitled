441d47d7043cf97e7d9cbd26c76b793c0638db46
程攀 <chengpancj@163.com>
2023-11-20 17:27:43 +0800

    Changes

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.println("Hello world!");
+        System.out.println("sdfsddfs");
+        System.out.println("sdfsddfs");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!11111");
+
+    }
+}
