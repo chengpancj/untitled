98fc9aa8ef7d3acb60bd8a48132a9d4e9444657b
程攀 <chengpancj@163.com>
2023-11-21 18:10:32 +0800

    Merge branch 'master' of https://github.com/chengpancj/untitled

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!11111");
+        System.out.println("Hello world!11111");
 
     }
 }
